1fe960fb4c3e3298f53229a03657e63c705b93a2
yseo4530 <yeon.seo@crownpeak.com>
2021-01-04 18:02:19 -0800

    remove unneeded auto-generated files and references to them

--- projects/cp-ngx-tui-image-editor/src/public-api.ts
+++ projects/cp-ngx-tui-image-editor/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of cp-ngx-tui-image-editor
  */
 
-export * from './lib/cp-ngx-tui-image-editor.service';
 export * from './lib/cp-ngx-tui-image-editor.component';
 export * from './lib/cp-ngx-tui-image-editor.module';
